ba63ba59c76f78e4a2b10b70ca04322d181ee078
Konrad Drozd <drozdkonrad@gmail.com>
2018-02-26 17:54:44 +0100

    Generate maven-quickstart-archetype.

---
+++ src/main/java/pwr/si/drozd/App.java
@@ -0,0 +1,13 @@
+package pwr.si.drozd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
