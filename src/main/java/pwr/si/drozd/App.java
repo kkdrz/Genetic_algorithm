c05b35c986deafa49a5ec3003fe0959dfa8c94ed
Konrad Drozd <drozdkonrad@gmail.com>
2018-03-04 23:32:34 +0100

    Calculating data to chart from many probes (REPEATS).

--- src/main/java/pwr/si/drozd/App.java
+++ src/main/java/pwr/si/drozd/App.java
@@ -11,69 +11,116 @@ import pwr.si.drozd.entity.Data;
 import pwr.si.drozd.algorithm.GeneticAlgorithm;
 import pwr.si.drozd.entity.Individual;
 import pwr.si.drozd.tools.DataReader;
+
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class App extends Application {
 
-    public static int POPULATION_SIZE = 1000;
-    public static int GENERATIONS = 200;
-    public static double MUTATION_RATE = 0.9;
+    public static int POPULATION_SIZE = 100;
+    public static int GENERATIONS = 20;
+    public static double MUTATION_RATE = 0.1;
     public static double CROSSING_RATE = 0.01;
-    public static long RANDOM_DURATION = 5000;
+    public static long RANDOM_DURATION = 1000;
+
+    public static int REPEATS = 10;
+
+    static List<Integer> randomTries = new ArrayList<>(REPEATS);
+    static List<Map<Integer, Integer>> bestTries = new ArrayList<>(REPEATS);
+    static List<Map<Integer, Integer>> worstTries = new ArrayList<>(REPEATS);
+    static List<Map<Integer, Integer>> avgTries = new ArrayList<>(REPEATS);
 
     static XYChart.Series randomSeries = new XYChart.Series();
     static XYChart.Series bestSeries = new XYChart.Series();
     static XYChart.Series bestPossibleSeries = new XYChart.Series();
-    static XYChart.Series avgSeries= new XYChart.Series();
-    static XYChart.Series worstSeries= new XYChart.Series();
+    static XYChart.Series avgSeries = new XYChart.Series();
+    static XYChart.Series worstSeries = new XYChart.Series();
 
 
     public static void main(String[] args) throws FileNotFoundException {
         DataReader dataReader = new DataReader();
         Data data = dataReader.readData("had14.txt");
-        GeneticAlgorithm GA = new GeneticAlgorithm(data);
+        initTriesLists();
 
-        GA.initPopulation();
-        GA.evaluate();
-        int currentGeneration = 0;
-        while(currentGeneration++ < GENERATIONS && GA.getBestIndividual().getCost() > data.getBestCost()) {
-            GA.selection();
-            GA.reproduction();
-            GA.mutation();
+        for (int i = 0; i < REPEATS; i++) {
+            GeneticAlgorithm GA = new GeneticAlgorithm(data);
+
+            GA.initPopulation();
             GA.evaluate();
-            populateDataToChart(data, GA, currentGeneration);
-            System.out.println(GA.getBestIndividual().calculateCost(data));
+            int currentGeneration = -1;
+            while (currentGeneration++ < GENERATIONS && GA.getBestIndividual().getCost() > data.getBestCost()) {
+                GA.selection();
+                GA.reproduction();
+                GA.mutation();
+                GA.evaluate();
+                bestTries.get(i).put(currentGeneration, GA.getBestIndividual().calculateCost(data));
+                worstTries.get(i).put(currentGeneration, GA.getWorstIndividual().calculateCost(data));
+                avgTries.get(i).put(currentGeneration, GA.getAverageCost());
+                System.out.println(GA.getBestIndividual().calculateCost(data));
+            }
+
+            RandomAlgorithm RA = new RandomAlgorithm(data);
+            Individual randomBest = RA.findBestIndividual(RANDOM_DURATION);
+            randomTries.add(randomBest.calculateCost(data));
         }
 
-        RandomAlgorithm RA = new RandomAlgorithm(data);
-        Individual randomBest = RA.findBestIndividual(RANDOM_DURATION);
-        for (int i = 0; i < GENERATIONS; i++) {
+        populateChartData(data);
 
-        }
-
-        populateConstValuesToChart(data, randomBest);
-
-        System.out.println("Random best: " + randomBest.calculateCost(data));
-        System.out.println(GA.getBestIndividual().calculateCost(data));
         launch(args);
     }
 
-    private static void populateConstValuesToChart(Data data, Individual randomBest) {
+    private static void populateChartData(Data data) {
+        bestSeries = calculateAvgOfRepeats(bestTries);
+        worstSeries = calculateAvgOfRepeats(worstTries);
+        avgSeries = calculateAvgOfRepeats(avgTries);
+        randomSeries = calculateAvgOfRepeatsList(randomTries);
+        populateConstValuesToChart(data);
+    }
+
+    private static void initTriesLists() {
+        initTriesList(bestTries);
+        initTriesList(worstTries);
+        initTriesList(avgTries);
+    }
+
+    private static void initTriesList(List<Map<Integer, Integer>> tries) {
+        for (int i = 0; i < REPEATS; i++) {
+            tries.add(new HashMap<Integer, Integer>());
+        }
+    }
+
+    private static XYChart.Series calculateAvgOfRepeatsList(List<Integer> tries) {
+        XYChart.Series series = new XYChart.Series();
+        int sum = 0;
+        for (Integer i : tries) {
+            sum += i;
+        }
+        int avg = sum / tries.size();
+        series.getData().add(new XYChart.Data(0, avg));
+        series.getData().add(new XYChart.Data(GENERATIONS, avg));
+        return series;
+    }
+
+    private static XYChart.Series calculateAvgOfRepeats(List<Map<Integer, Integer>> tries) {
+        XYChart.Series series = new XYChart.Series();
+        for (int j = 0; j < GENERATIONS; j++) {
+            int avgCost = 0;
+            for (int i = 0; i < REPEATS; i++) {
+                avgCost += tries.get(i).get(j);
+            }
+            series.getData().add(new XYChart.Data(j, avgCost / REPEATS));
+        }
+        return series;
+    }
+
+    private static void populateConstValuesToChart(Data data) {
         bestPossibleSeries.getData().add(new XYChart.Data(0, data.getBestCost()));
         bestPossibleSeries.getData().add(new XYChart.Data(GENERATIONS, data.getBestCost()));
-
-        randomSeries.getData().add(new XYChart.Data(0, randomBest.calculateCost(data)));
-        randomSeries.getData().add(new XYChart.Data(GENERATIONS, randomBest.calculateCost(data)));
     }
-
-    private static void populateDataToChart(Data data, GeneticAlgorithm GA, int currentGeneration) {
-        bestSeries.getData().add(new XYChart.Data(currentGeneration, GA.getBestIndividual().calculateCost(data)));
-        worstSeries.getData().add(new XYChart.Data(currentGeneration, GA.getWorstIndividual().calculateCost(data)));
-        avgSeries.getData().add(new XYChart.Data(currentGeneration, GA.getAverageCost()));
-
-    }
-
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -87,16 +134,16 @@ public class App extends Application {
         final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
         yAxis.setAutoRanging(false);
-        yAxis.setLowerBound(((XYChart.Data<Number, Number>)bestPossibleSeries.getData().get(0)).getYValue().doubleValue() - 300.0);
-        yAxis.setUpperBound(((XYChart.Data<Number, Number>)worstSeries.getData().get(0)).getYValue().doubleValue() + 300.0);
+        yAxis.setLowerBound(((XYChart.Data<Number, Number>) bestPossibleSeries.getData().get(0)).getYValue().doubleValue() - 300.0);
+        yAxis.setUpperBound(((XYChart.Data<Number, Number>) worstSeries.getData().get(0)).getYValue().doubleValue() + 300.0);
 
         xAxis.setLabel("Generation");
-        final LineChart<Number,Number> lineChart =
-                new LineChart<>(xAxis,yAxis);
+        final LineChart<Number, Number> lineChart =
+                new LineChart<>(xAxis, yAxis);
 
         lineChart.setTitle("Genetic Algorithm");
 
-        Scene scene  = new Scene(lineChart,800,600);
+        Scene scene = new Scene(lineChart, 800, 600);
         lineChart.getData().addAll(bestSeries, worstSeries, avgSeries, randomSeries, bestPossibleSeries);
 
         stage.setScene(scene);
